4ea7389750f3c2895756ab576a3176a25e457473
arno <arnaud.martin@epfeud.f>
2022-12-05 15:05:50 +0100

    added google prefix

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+    pre = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WEKFJ1L7K3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WEKFJ1L7K3');
+</script>
+
+"""
+    return pre + "Hello World"
