e73bab6355f17cafd513c9f86ffabec1813fd279
arno <arnaud.martin@epfeud.f>
2022-12-05 15:37:13 +0100

    google analytics works

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    pre = """<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WEKFJ1L7K3"></script>
+    pre = """
+<!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-WEKFJ1L7K3"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WEKFJ1L7K3');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-WEKFJ1L7K3');
 </script>
-
 """
     return pre + "Hello World"
+    
+   
